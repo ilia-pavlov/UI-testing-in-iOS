62321f1dad79ff9a922093940e3a9c294595e0c4
ilia Pavlov <3472204455ilia@gmail.com>
2021-12-28 12:36:36 -0500

    singleRun

--- Script.sh
+++
@@ -1,8 +0,0 @@
-xcodebuild \
-  -project UITestingTutorial.xcodeproj \
-  -scheme UITestingTutorial \
-  -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 8,OS=15.2' \
-  test \
-  | xcbeautify
-
